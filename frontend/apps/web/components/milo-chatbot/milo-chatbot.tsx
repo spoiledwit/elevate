07a4da5ac8bee6f64e8e209b125ea7ba44e21749
Ahmad Ashfaq <spoiledwit@gmail.com>
2025-09-29 17:43:31 +0500

    fix: fixed a bunch of stuff

--- frontend/apps/web/components/milo-chatbot/milo-chatbot.tsx
+++ frontend/apps/web/components/milo-chatbot/milo-chatbot.tsx
@@ -159,7 +159,7 @@ export function MiloChatbot(props: MiloChatbotProps = {}) {
             </div>
           </div>
           <button
-            onClick={() => setIsOpen(false)}
+            onClick={handleClose}
             className="p-2.5 hover:bg-white/10 rounded-xl transition-all duration-300 group"
           >
             <X className="w-5 h-5 text-gray-400 group-hover:text-white transition-colors" />

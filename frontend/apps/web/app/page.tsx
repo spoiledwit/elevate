42ae8170df0cf47dcd105ae3a746c1a471a75382
MTalha250 <talhabinay@gmail.com>
2025-10-07 22:07:25 +0500

    update

--- frontend/apps/web/app/page.tsx
+++ frontend/apps/web/app/page.tsx
@@ -6,8 +6,10 @@ import Pricing from "@/components/home/Pricing";
 import { Footer } from "@/components/landing/footer";
 import ProblemSol from "@/components/home/ProblemSol";
 import WhyIBuilt from "@/components/home/WhyIBuilt";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/login");
   return (
     <div className="min-h-screen">
       <Navbar />

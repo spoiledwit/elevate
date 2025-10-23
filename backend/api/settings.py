54e7045c4cc273a0562f5f320c3e48920596d036
Ahmad Ashfaq <spoiledwit@gmail.com>
2025-10-23 22:20:52 +0500

    fix: licensing removal

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
         "PASSWORD": environ.get("DATABASE_PASSWORD", "change-password"),
         "NAME": environ.get("DATABASE_NAME", "db"),
         "HOST": environ.get("DATABASE_HOST", "db"),
-        "PORT": "5432",
+        "PORT": environ.get("DATABASE_PORT", "5432"),
         "TEST": {
             "NAME": "test",
         },
